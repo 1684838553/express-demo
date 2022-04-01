a7cdd4b824ab30a5db60da4c2f1f51581755489f
drunk喵咪 <1684838553@qq.com>
2022-04-01 10:28:11 +0800

    Update profile.js

--- realword-api-express/router/profile.js
+++ realword-api-express/router/profile.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const contro = require('../controller/profile')
+const auth = require('../middleware/auth')
 
-// 获取用户资料
-router.get('/:username', contro.getUserByName)
+// 获取文章列表
+router.get('/', auth, contro.getArticles)
 
-// 关注用户
-router.post('/:username/follow', contro.focusUser)
+// 获取用户关注的作者文章列表
+router.get('/feed', auth, contro.getFeedArticles)
 
-// 取消关注用户
-router.delete('/:username/follow', contro.cancelUser)
+// 获取文章
+router.get('/articles/:slug', contro.getArticlesBySulg)
+
+// 创建文章
+router.post('/articles', auth, contro.postArticles)
+
+// 更新文章
+router.put('/articles/:slug', auth, contro.putArticle)
+
+// 删除文章
+router.delete('/articles/:slug', auth, contro.deleteArticle)
+
+// 添加文章评论
+router.post('/articles/:slug/comments', auth, contro.postArticlesComments)
+
+// 获取文章评论列表
+router.get('/articles/:slug/comments', auth, contro.getArticlesComments)
+
+// 删除文章评论
+router.delete('/articles/:slug/comments/:id', auth, contro.deleteArticleComment)
 
 module.exports = router
