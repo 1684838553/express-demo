c0e85cc8ff6e7bd25e942d864dcbbba8171e9789
drunk喵咪 <1684838553@qq.com>
2022-04-08 17:11:39 +0800

    Update index.js

--- express-ssr/router/index.js
+++ express-ssr/router/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-// 用户相关路由
-router.use(require("./user"));
+// // 用户相关路由
+// router.use(require("./user"));
 
-// 用户资料相关路由
-router.use("/profiles", require("./profile"));
-
+// // 用户资料相关路由
+// router.use("/profiles", require("./profile"));
+router.get('/',(req,res)=>{
+    res.render('index.art')
+})
 module.exports = router;
