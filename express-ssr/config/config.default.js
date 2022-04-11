57450cd15d92bee2046bed7b76a02c743d1fbbd4
drunk喵咪 <1684838553@qq.com>
2022-04-11 17:05:06 +0800

    Update config.default.js

--- express-ssr/config/config.default.js
+++ express-ssr/config/config.default.js
@@ -2,4 +2,6 @@ module.exports = {
   dbUri: "mongodb://localhost:27017/realword",
   // uuid
   jwtSecret: "511cac3d-1ad9-403e-96f6-a544a013f2c8",
+  sessionSecret: '453031d9-dbf1-4a24-880b-b8e2274acc8a'
 };
+
