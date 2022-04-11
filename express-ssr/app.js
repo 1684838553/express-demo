9cc6570f9c7b72a9c37b3e59029ebd614bfc21d0
drunk喵咪 <1684838553@qq.com>
2022-04-11 17:15:01 +0800

    Update app.js

--- express-ssr/app.js
+++ express-ssr/app.js
@@ -4,20 +4,45 @@ const path = require('path')
 const router = require("./router/index");
 const morgan = require("morgan");
 const errorhandler = require('errorhandler')
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
+const { sessionSecret } = require('./config/config.default')
+const { dbUri } = require('./config/config.default')
 require("./model/index");
+
 
 // 配置解析表单请求体：application/json
 app.use(express.json());
 
 // 配置解析表单请求体：application/x-www-form-urlencoded
 app.use(express.urlencoded({
-  extended:true
+  extended: true
 }));
+
+// 配置使用 Session 中间件
+app.use(session({
+  secret: sessionSecret,  // 签发 session id 的密钥
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 // 过期时间，单位毫秒
+    // secure: true // 只有 https协议才会收发cookie
+  }, // 保存 session id 的Cookie设置
+  store: MongoStore.create({
+    mongoUrl: dbUri
+  })  // 将数据持久化到 MonogoDB 数据库中
+}))
+
+app.use((req, res, next) => {
+  // 统一给模板添加数据
+  app.locals.sessionUser = req.session.user
+  next()
+})
 
 // 当渲染以art结尾的资源文件时，使用express-art-template处理
 app.engine('art', require('express-art-template'));
 app.set('view options', {
-    debug: process.env.NODE_ENV !== 'production'
+  debug: process.env.NODE_ENV !== 'production'
 });
 // 配置模板文件的存储目录
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +50,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'art');
 
 
-app.use('./public',express.static(path.join(__dirname,'./public')))  
+app.use('/public', express.static(path.join(__dirname, './public')))
+app.use('/node_modules', express.static(path.join(__dirname, './node_modules')))
 
 // 日志输出
 app.use(morgan("dev"));
