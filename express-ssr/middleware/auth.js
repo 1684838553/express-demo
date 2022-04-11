c065039bad37268dbe77a2f89afdc9278a8c52bf
drunk喵咪 <1684838553@qq.com>
2022-04-11 17:06:07 +0800

    Update auth.js

--- express-ssr/middleware/auth.js
+++ express-ssr/middleware/auth.js
@@ -1,23 +1,11 @@
-// 认证身份
-const { verify } = require('../util/jwt')
-const { jwtSecret } = require('../config/config.default')
-const { User } = require('../model')
+module.exports = async (req, res, next) => {
+    // 检查有没有 Session User
+    const sessionUser = req.session.user
 
-module.exports =async (req, res, next) => {
-    let token = req.headers.authorization
-
-    token = token ? token.split('Bearer ')[1] : null
-
-    if (!token) {
-        return res.status(401).end()
+    if (sessionUser) {
+        return next()
     }
 
-    try {
-        const decodedToken = await verify(token, jwtSecret)
-        // 验证token正确，返回用户信息
-        req.user = await User.findById(decodedToken.userId)
-        next()
-    } catch (err) {
-        return res.status(401).end()
-    }
+    // 重定向到登录页
+    res.redirect('/login')
 }
