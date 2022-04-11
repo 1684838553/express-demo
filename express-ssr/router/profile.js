fc1b80232aa0e9b952f1a8a84cbb0be55eb821c7
drunk喵咪 <1684838553@qq.com>
2022-04-11 17:09:07 +0800

    Update profile.js

--- express-ssr/router/profile.js
+++ express-ssr/router/profile.js
@@ -4,31 +4,12 @@ const contro = require('../controller/profile')
 const auth = require('../middleware/auth')
 const profileValidator = require("../validator/profile");
 
-// 获取文章列表
-router.get('/', auth, contro.getArticles)
+router.get('/', contro.showHome)
 
-// 获取用户关注的作者文章列表
-router.get('/feed', auth, contro.getFeedArticles)
+router.get('/editor', auth, contro.showEditor)
 
-// 获取文章
-router.get('/articles/:articleId', profileValidator.getArticle, contro.getArticlesBySulg)
+router.get('/editor/:articleId', auth, contro.showEditor)
 
-// 创建文章
-router.post('/articles', auth, profileValidator.createAticle, contro.postArticles)
-
-// 更新文章
-router.put('/articles/:articleId', auth, profileValidator.updateArticle, contro.putArticle)
-
-// 删除文章
-router.delete('/articles/:articleId', auth, profileValidator.deleteArticle, contro.deleteArticle)
-
-// 添加文章评论
-router.post('/articles/:articleId/comments', auth, contro.postArticlesComments)
-
-// 获取文章评论列表
-router.get('/articles/:articleId/comments', auth, contro.getArticlesComments)
-
-// 删除文章评论
-router.delete('/articles/:articleId/comments/:id', auth, contro.deleteArticleComment)
+router.get('/article/:articleId', auth, contro.showArticle)
 
 module.exports = router
